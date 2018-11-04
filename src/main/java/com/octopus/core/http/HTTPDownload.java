2b74383ab88458ec8d7e2b65fa60a522a0774d89
Kasun Vithanage <alankasun@gmail.com>
2018-11-04 11:27:28 +0530

    added copyright

---
+++ src/main/java/com/octopus/core/http/HTTPDownload.java
@@ -0,0 +1,83 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 octopusdownloader
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.octopus.core.http;
+
+import com.octopus.core.Downloadable;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Path;
+
+public class HTTPDownload implements Downloadable {
+    private URL url;
+    private Path file;
+    private long size;
+    private long from;
+    private long to;
+    private long receivedBytes;
+
+    public HTTPDownload(URL url, Path file, long size, long from, long to) {
+        this.url = url;
+        this.file = file;
+        this.size = size;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void download() throws Exception {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setInstanceFollowRedirects(false);
+        urlConnection.setRequestProperty("Range", getRange());
+        urlConnection.connect();
+        int respCode = urlConnection.getResponseCode();
+
+        switch (respCode) {
+            case 416:
+                throw new HTTPOutOfRangeException("Out of range");
+
+            case 200:
+            case 206:
+                startDownload();
+                break;
+
+            default:
+                throw new HTTPException("HTTP Error " + respCode);
+        }
+    }
+
+    private void startDownload() {
+
+    }
+
+    @Override
+    public long receivedBytes() {
+        return receivedBytes;
+    }
+
+    String getRange() {
+        return "bytes=" + from + "-" + (to == 0 ? "" : to);
+    }
+}
